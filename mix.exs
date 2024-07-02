2c4b8f70bcd8ba51956b10f161ddc4020fa724ce
Gabriel Kolber <gabrielkolber@icloud.com>
2024-07-01 22:34:23 -0300

    chore: update dependencies configuration
    - Added runtime: false for patch and ex_doc dependencies.
    - Ensured patch is used only for tests and ex_doc only for documentation generation.

--- mix.exs
+++ mix.exs
@@ -8,30 +8,31 @@ defmodule Cachetastic.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description:
+        "A unified caching library for Elixir with support for ETS and Redis backends.",
       package: package(),
       name: "Cachetastic",
       source_url: "https://github.com/gskolber/cachetastic",
       homepage_url: "https://github.com/gskolber/cachetastic",
       docs: [
-        # Specify the main page in the docs
         main: "readme",
         extras: ["README.md"]
       ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :patch]
+      extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:redix, "~> 1.0"},
-      {:patch, "~> 0.12.0", only: :test},
+      # Patch is used only for tests, not needed at runtime
+      {:patch, "~> 0.12.0", only: :test, runtime: false},
+      # ExDoc is used only for documentation generation
       {:ex_doc, "~> 0.23", only: :dev, runtime: false}
     ]
   end
